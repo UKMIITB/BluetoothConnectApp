056f3fbe06de90c3bd71aac97660f47665260d7d
Ujjwal Kumar <ujjwalkr.maharana@gmail.com>
2021-04-23 15:55:03 +0530

    mainActivity

--- app/src/main/java/com/example/bluetoothconnect/MainActivity.java
+++ app/src/main/java/com/example/bluetoothconnect/MainActivity.java
@@ -1,17 +1,44 @@
 package com.example.bluetoothconnect;
 
-import android.content.Intent;
+import android.content.ComponentName;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.media.MediaBrowserCompat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaBrowserCompat mMediaBrowser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, ThirdActivity.class));
+        init();
+        playRingtone();
+    }
+
+    private void init() {
+        mMediaBrowser = new MediaBrowserCompat(this, new ComponentName(this, MediaPlaybackService.class),
+                new MediaBrowserCompat.ConnectionCallback(), null);
+        mMediaBrowser.connect();
+    }
+
+    private void playRingtone() {
+
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone ringTone = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        ringTone.play();
+        ringTone.setLooping(true);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMediaBrowser.disconnect();
+        super.onDestroy();
     }
 }
